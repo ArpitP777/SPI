3e2215c82f1b1e8dac46e379c64b3b8ed6dc1e1b
Arpit Prakash <thegreatkingarpit@gmail.com>
2026-07-21 02:50:03 +0530

    ports reassigned

--- RTL/SPI.v
+++ RTL/SPI.v
@@ -4,12 +4,18 @@ module spi_master (
     input clk,
     input rst,
     output sclk,
-    output cs,
+    output reg cs,
     input miso,
-    output mosi
+    output reg mosi,
+    input [7:0] data_tx,
+    output [7:0] data_rx,
+    input start,
+    output reg tx_busy,
+    output reg done
 );
 
-    reg tx_busy;
-    reg done;
+    reg [7:0] tx_shift_reg;
+    reg [7:0] rx_shift_reg;
+    reg [2:0] bit_c;
 
 endmodule
