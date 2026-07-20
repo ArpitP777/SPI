b3255ca005a35eb8117465ee7beecb09a0c2924b
Arpit Prakash <thegreatkingarpit@gmail.com>
2026-07-21 02:40:07 +0530

    ports

--- RTL/SPI.v
+++ RTL/SPI.v
@@ -1,7 +1,15 @@
 `timescale 1ns/1ps
 
-module SPI ();
+module spi_master (
+    input clk,
+    input rst,
+    output sclk,
+    output cs,
+    input miso,
+    output mosi
+);
 
-    
+    reg tx_busy;
+    reg done;
 
 endmodule
