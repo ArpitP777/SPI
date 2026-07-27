cf8e1eb4c707378c8ce833417b04682893498de1
Arpit Prakash <thegreatkingarpit@gmail.com>
2026-07-27 23:20:44 +0530

    bruh

--- RTL/clk_divide.v
+++ RTL/clk_divide.v
@@ -7,17 +7,16 @@ module clk_divide (
 );
 
     initial sclk = 1'b0;
-    
-    reg [5:0] cnt1 = 0;
-    reg [5:0] cnt2 = 0;
+
+    reg [5:0] cnt = 0;
     
     always@(posedge clk) begin
-        if(cnt2 - cnt1 == 50) begin
+        if(cnt == 50) begin
             sclk <= ~sclk;
-            cnt2 <= 0;
+            cnt <= 0;
         end
         else begin
-            cnt2 <= cnt2 + 1;
+            cnt <= cnt + 1;
         end
     end
 endmodule
